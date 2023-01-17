8d793ee644fb691553d7c36e19124a32613e9739
ylose <heungeob1003@gmail.com>
2023-01-17 17:52:33 +0900

    웹에서 데이터를 가져오는 부분인 provider 생성

---
+++ lib/providers/news_provider.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http_tutorial/models/news.dart';
+import 'package:http/http.dart' as http;
+
+class NewsProviders{
+  Uri uri = Uri.parse("https://newsapi.org/v2/top-headlines?country=kr&apiKey=d198544121114b95a2377765e7f80ea1");
+
+  Future<List<News>> getNews() async {
+    List<News> news = [];
+
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      news = jsonDecode(response.body)['articles'].map<News>( (article) {
+        return News.fromMap(article);
+      }).toList();
+    }
+
+    return news;
+  }
+
+
+}
