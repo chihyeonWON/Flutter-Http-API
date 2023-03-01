7d14d7b5b6df009a3c3cef28efdd04e740d38f8a
ylose <heungeob1003@gmail.com>
2023-03-01 17:29:29 +0900

    author, url, publishedAt(출판사, url, 출판일자) 추가

--- lib/models/news.dart
+++ lib/models/news.dart
@@ -1,14 +1,23 @@
 class News{
-  late String title;
-  late String content;
+  late String author; // 출판사
+  late String title; // 주제
+  late String content; // 내용
+  late String url; // url
+  late String publishedAt; // 일자
 
   News({
+    required this.author,
     required this.title,
-    required this.content
+    required this.content,
+    required this.url,
+    required this.publishedAt
   });
 
   News.fromMap(Map<String, dynamic>? map) {
+    author = map?['author'] ?? '';
     title = map?['title'] ?? '';
     content = map?['description'] ?? '';
+    url = map?['url'] ?? '';
+    publishedAt = map?['publishedAt'] ?? '';
   }
 }
