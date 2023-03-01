a8a355acf2479a54cbf9f689ce5241deb955be1d
ylose <heungeob1003@gmail.com>
2023-03-01 17:29:47 +0900

    author, url, publishedAt(출판사, url, 출판일자) 추가와 UI 수정

--- lib/widgets/news_widget.dart
+++ lib/widgets/news_widget.dart
@@ -1,11 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../providers/news_provider.dart';
-
 import '../models/news.dart';
+import '../providers/news_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsScreen extends StatefulWidget {
-
   @override
   _NewsScreenState createState() {
     return new _NewsScreenState();
@@ -13,7 +11,6 @@ class NewsScreen extends StatefulWidget {
 }
 
 class _NewsScreenState extends State<NewsScreen> {
-
   List<News> news = [];
   bool isLoading = true;
   NewsProviders newsProvider = NewsProviders();
@@ -35,30 +32,73 @@ class _NewsScreenState extends State<NewsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("뉴스 http"),
+        title: Text("한국을 보는 창"),
       ),
-      body: isLoading ? Center(child: const CircularProgressIndicator(),) :
-      GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1 / 3,
-          crossAxisSpacing: 5,
-          mainAxisSpacing: 5,
-      ),
-          itemCount: news.length,
-          itemBuilder: (context, index) {
-            return Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Text(news[index].title,
-                      style:TextStyle(
-                          fontWeight: FontWeight.bold)),
-                  Text(news[index].content)
-                ],
-              ),
-            );
-          }),
+      body: isLoading
+          ? Center(
+              child: const CircularProgressIndicator(),
+            )
+          : GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                  childAspectRatio: 2 / 3,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20),
+              itemCount: news.length,
+              itemBuilder: (context, index) {
+                final Uri _url = Uri.parse(news[index].url);
+                return Container(
+                  height:MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 3.0,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(20),
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('[${news[index].author}]',
+                            style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Padding(
+                          padding: const EdgeInsets.only(top:30.0),
+                          child: Text(news[index].title,
+                              style: TextStyle(
+                                  fontSize: 20.0, fontWeight: FontWeight.bold)),
+                        ),
+                        Text(
+                          news[index].content,
+                          maxLines: 5,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          )
+                        ),
+                        Text('아래 링크를 클릭하시면 자세한 내용을 볼 수 있습니다.',style:TextStyle(fontSize:15.0, fontWeight:FontWeight.bold,)),
+                        InkWell(
+                            child: Text(
+                              news[index].url,
+                              style:TextStyle(
+                                fontSize:16.0,
+                              )
+                            ),
+                            onTap: () => launchUrl(_url)),
+                        Text('일자 : ${news[index].publishedAt}',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight:FontWeight.bold,
+                            )),
+                      ],
+                    ),
+                  ),
+                );
+              }),
     );
   }
 }
